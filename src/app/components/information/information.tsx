af0c287119c6da99f100ae8af7cc01f452555222
Pedro Aluisio <121454190+pedrinGrando@users.noreply.github.com>
2023-12-22 11:57:08 -0300

    Teste

---
+++ src/app/components/information/information.tsx
@@ -0,0 +1,20 @@
+import { SectionTitle } from "../sectionTitle/section-title"
+import "./information.scss"
+
+
+export function Info(){
+    return(
+        <div className="infos">
+        <SectionTitle text="Languages"/>
+        <div className="languages-info">
+          <span>🇺🇸 EN - Advanced</span>
+          <span>🇧🇷 PT-BR - Native Speaker</span>
+        </div>
+        <SectionTitle text="Education"/>
+        <div className="educational-info">
+          <span>🎓</span>
+          <span>Analysis and systems development at SENAC - SC</span>
+        </div>
+      </div>
+    )
+}
